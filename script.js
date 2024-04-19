d6427ae126ea81449d695016a1b115dfb1ba771e
kimya-h <kimya.mailbox@gmail.com>
2024-04-20 01:03:25 +0330

    finish

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const  poke_container = document.getElementById('poke-container')
 const pokemon_count = 150
-const color = {
+const colors = {
     fire : '#FDDFDF',
     grass: '#DEFDE0',
     electric: '#FCF7DE',
@@ -18,6 +18,10 @@ const color = {
 
 }
 
+const main_types = Object.keys(colors)
+
+
+
 const fetchPokemons = async () => {
     for(let i = 1; i <= pokemon_count; i++){
        await getPokemon(i)
@@ -28,7 +32,37 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+ 
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonInnerHtml = `
+    <div class="img-container">
+         <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
+    `
+//images
+    pokemonEl.innerHTML = pokemonInnerHtml
+
+    poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemons()
